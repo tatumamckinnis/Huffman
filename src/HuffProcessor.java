435af3b6facbe8f94b9878b6649f5435e252c06b
owen astrachan <ola@cs.duke.edu>
2023-11-15 21:20:14 -0500

    first for huff

---
+++ src/HuffProcessor.java
@@ -0,0 +1,97 @@
+import java.util.PriorityQueue;
+
+/**
+ * Although this class has a history of several years,
+ * it is starting from a blank-slate, new and clean implementation
+ * as of Fall 2018.
+ * <P>
+ * Changes include relying solely on a tree for header information
+ * and including debug and bits read/written information
+ * 
+ * @author Owen Astrachan
+ *
+ * Revise
+ */
+
+public class HuffProcessor {
+
+	private class HuffNode implements Comparable<HuffNode> {
+		HuffNode left;
+		HuffNode right;
+		int value;
+		int weight;
+
+		public HuffNode(int val, int count) {
+			value = val;
+			weight = count;
+		}
+		public HuffNode(int val, int count, HuffNode ltree, HuffNode rtree) {
+			value = val;
+			weight = count;
+			left = ltree;
+			right = rtree;
+		}
+
+		public int compareTo(HuffNode o) {
+			return weight - o.weight;
+		}
+	}
+
+	public static final int BITS_PER_WORD = 8;
+	public static final int BITS_PER_INT = 32;
+	public static final int ALPH_SIZE = (1 << BITS_PER_WORD); 
+	public static final int PSEUDO_EOF = ALPH_SIZE;
+	public static final int HUFF_NUMBER = 0xface8200;
+	public static final int HUFF_TREE  = HUFF_NUMBER | 1;
+
+	private boolean myDebugging = false;
+	
+	public HuffProcessor() {
+		this(false);
+	}
+	
+	public HuffProcessor(boolean debug) {
+		myDebugging = debug;
+	}
+
+	/**
+	 * Compresses a file. Process must be reversible and loss-less.
+	 *
+	 * @param in
+	 *            Buffered bit stream of the file to be compressed.
+	 * @param out
+	 *            Buffered bit stream writing to the output file.
+	 */
+	public void compress(BitInputStream in, BitOutputStream out){
+
+
+		// remove all this code when implementing compress
+		while (true){
+			int val = in.readBits(BITS_PER_WORD);
+			if (val == -1) break;
+			out.writeBits(BITS_PER_WORD, val);
+		}
+		out.close();
+	}
+
+	/**
+	 * Decompresses a file. Output file must be identical bit-by-bit to the
+	 * original.
+	 *
+	 * @param in
+	 *            Buffered bit stream of the file to be decompressed.
+	 * @param out
+	 *            Buffered bit stream writing to the output file.
+	 */
+	public void decompress(BitInputStream in, BitOutputStream out){
+
+		// remove all code when implementing decompress
+
+		while (true){
+			int val = in.readBits(BITS_PER_WORD);
+			if (val == -1) break;
+			out.writeBits(BITS_PER_WORD, val);
+		}
+		out.close();
+	}
+}
